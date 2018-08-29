3ef182b02035fcfbf8fb80325f03a36907c3e4fc
Angel Ballesteros <angel.ballesteros@flywire.com>
2018-08-29 08:59:12 +0200

    Adds prompt config

---
+++ shell/bashrc/promp_configuration.sh
@@ -0,0 +1,44 @@
+##########################################
+# PROMPT CONFIGURATION
+##########################################
+
+# uncomment for a colored prompt, if the terminal has the capability; turned
+# off by default to not distract the user: the focus in a terminal window
+# should be on the output of commands, not on the prompt
+force_color_prompt=yes
+
+if [ -n "$force_color_prompt" ]; then
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+  # We have color support; assume it's compliant with Ecma-48
+  # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+  # a case would tend to support setf rather than setaf.)
+  color_prompt=yes
+    else
+  color_prompt=
+    fi
+fi
+
+WHITE_BLACK='\[\033[38;5;15;48;5;0m\]'
+WHITE_BLUE='\[\033[38;5;15;48;5;27m\]'
+BLACK_BLUE='\[\033[38;5;0;48;5;27m\]'
+RESET='\[\033[00m\]'
+BLUE_YELLOW='\[\033[38;5;27;48;5;226m\]'
+BLACK_YELLOW='\[\033[38;5;0;48;5;226m\]'
+YELLOW_TRANSPARENT='\[\033[38;5;226;49m\]'
+YELLOW_TRANSPARENT='\[\033[38;5;118;49m\]'
+WHITE_ORANGE='\[\033[38;5;15;48;5;208m\]'
+BLACK_LIME='\[\033[38;5;0;48;5;118m\]'
+BLUE_LIME='\[\033[38;5;27;48;5;118m\]'
+LIME_TRANSPARENT='\[\033[38;5;118;49m\]'
+
+parse_git_branch() {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\ \1/'
+}
+
+if [ "${USER}" == "angel.ballesterosflywire.com" ]; then
+  export PS1="${WHITE_BLACK}╭ "'\h '"${BLACK_BLUE}▶${RESET}${WHITE_BLUE}"'\w '"${RESET}${BLUE_YELLOW}▶${RESET}${BLACK_YELLOW}"'$(parse_git_branch)'" ${YELLOW_TRANSPARENT}▶${RESET}
+${WHITE_BLACK}╰▶ ${RESET} "
+else
+  export PS1="${WHITE_BLACK}╭ "'\h '"${BLACK_BLUE}▶${RESET}${WHITE_BLUE}"'\w '"${RESET}${BLUE_LIME}▶${RESET}${BLACK_LIME}"'$(parse_git_branch)'" ${LIME_TRANSPARENT}▶${RESET}
+${WHITE_BLACK}╰▶ ${RESET} "
+fi
