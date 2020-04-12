6388ad5b4d3581317cbdaa47522eac098080bade
Angel Ballesteros <angel.ballesteros@flywire.com>
2020-04-12 10:44:34 +0200

    Exports COMPOSE_DOCKER_CLI_BUILD=1

--- shell/bash_profile.sh
+++ shell/bash_profile.sh
@@ -39,6 +39,7 @@ fi
 export PT_VM_MEMORY=4096
 export VICTORIA_BIN=~/gitlab/tools/victoria/bin/victoria
 export PATH=$PATH:~/go/bin
+export COMPOSE_DOCKER_CLI_BUILD=1
 
 source ~/.fresh/build/shell.sh
 
